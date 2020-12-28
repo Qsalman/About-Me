6d2e662596d0aa7cbdb79fdc052a10f0c2cb166a
Qsalman <qosaisalman@gmail.com>
2020-12-28 19:09:38 +0200

    lab02

---
+++ js/app.js
@@ -0,0 +1,113 @@
+'use strict'; // use strict mode to prevent doing mistakes 
+
+
+if (confirm('Have you Ever Meet Me?(yes or no)')) {
+   
+   
+ //console.log('Great');
+ alert("Great");
+  } else {
+    
+    //console.log('Maybe Another time');
+   alert("Maybe Another time");
+  }
+
+
+
+
+
+  if (confirm('Do you know where im From ?(yes or no)')) {
+   
+   
+    //console.log('Great');
+    alert("Great");
+     } else {
+       
+       //console.log('Maybe Another time');
+      alert("Im From Palestine");
+     }
+
+
+     if (confirm('Do you know whats my major is ?(yes or no)')) {
+   
+   
+        //console.log('Great');
+        alert("Great");
+         } else {
+           
+           //console.log('Maybe Another time');
+          alert("Im a Devloper");
+         }
+
+
+
+         if (confirm('Do you know How Old am i  ?(yes or no)')) {
+   
+   
+            //console.log('Great');
+            alert("Great");
+             } else {
+               
+               //console.log('Maybe Another time');
+              alert("Im 26");
+             }
+
+
+             if (confirm('Do you if im a student  ?(yes or no)')) {
+   
+   
+                //console.log('Great');
+                alert("Great");
+                 } else {
+                   
+                   //console.log('Maybe Another time');
+                  alert("Im Student of Software Dvelopment ");
+                 }
+
+
+
+var favoritefood = prompt('whats my Faverate food ? (Shawarma /Burger)');
+console.log(favoritefood);
+console.log(favoritefood.toLowerCase());
+console.log(favoritefood.toUpperCase());
+
+
+var favoritefood = prompt("did you eat Burger today (Yes, No)"); 
+if(ufavoritefood.toLowerCase()=== "yes" ){
+    alert('Great :)');
+} else {
+    alert('you should its Tasty');
+}
+
+
+var Height = prompt('whats my Height ? (190 /170)');
+if (Height.toLocaleLowerCase()=== '190'){
+
+    alert('Correct');}
+else {(fHeight.toLocaleLowerCase()=== '170')
+alert('Try Again');
+}
+
+
+// console.log(Height);
+// console.log(Height.toLowerCase());
+// console.log(Height.toUpperCase());
+
+var favoritefood = prompt("did you eat Burger today (Yes, No)"); 
+if(ufavoritefood.toLowerCase()=== "yes" ){
+    alert('Great :)');
+} else {
+    alert('you should its Tasty');
+}
+
+var myhair  = prompt('is my hair long ? (long /not long)');
+//console.log(myhair);
+//console.log(myhair.toLowerCase());
+//console.log(myhair.toUpperCase());
+if (myhair.toLocaleLowerCase()=== 'long'){
+
+    alert('Correct');}
+else {(fmyhair.toLocaleLowerCase()=== 'not long')
+alert('Try Again');
+}
+
